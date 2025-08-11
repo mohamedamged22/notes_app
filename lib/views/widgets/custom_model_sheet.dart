8688e3f8f62e95347944ae01382475d16501166e
mohamedamged22 <mohamedamgedzena@gmail.com>
2025-08-11 16:05:40 +0300

    create custom button and finsh bottom sheet

--- lib/views/widgets/custom_model_sheet.dart
+++ lib/views/widgets/custom_model_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class CustomModelSheet extends StatelessWidget {
@@ -8,13 +9,18 @@ class CustomModelSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hintText: 'Title'),
-          SizedBox(height: 16),
-          CustomTextField(hintText: 'Content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hintText: 'Content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomButton(),
+            SizedBox(height: 32),
+          ],
+        ),
       ),
     );
   }
