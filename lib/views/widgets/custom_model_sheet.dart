26528e8cf4c13ef24f3bda25dc619e2a90f0b90b
mohamedamged22 <mohamedamgedzena@gmail.com>
2025-08-13 08:03:20 +0300

    fix keyboard

--- lib/views/widgets/custom_model_sheet.dart
+++ lib/views/widgets/custom_model_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
 
@@ -24,7 +23,9 @@ class CustomModelSheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading? true:false,
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: EdgeInsets.only(left: 16.0,right: 16,
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
               child: SingleChildScrollView(child: AddNoteForm()),
             ),
           );
